703430f526a4422b056afd6ce4502f9ca6c46130
Ben <hnguyent@andrew.cmu.edu>
2026-01-16 03:51:38 -0500

    Basic outline of main arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+use clap::Parser;
+use std::{
+    fs::{File, metadata},
+    io::{BufReader, prelude::*}, path,
+};
+
+#[derive(Parser)]
+struct Cli {
+    command: String,
+    path: std::path::PathBuf,
+}
+
+fn init(path: &std::path::PathBuf) -> anyhow::Result<()> {
+    todo!("Implement initialization logic");
+}
+
+fn check(path: &std::path::PathBuf) -> anyhow::Result<()> {
+    todo!("Implement integrity checking");
+}
+
+fn update(path: &std::path::PathBuf) -> anyhow::Result<()> {
+    todo!("Implement updating mechanism");
+}
+
+fn main() -> anyhow::Result<()> {
+    let stdout = std::io::stdout();
+    let mut handle = std::io::BufWriter::new(stdout);
+
+    let args = Cli::parse();
+
+    match args.command.as_str() {
+        "init" => {
+            init(&args.path)
+        }
+        "check" => {
+            check(&args.path)
+        }
+        "update" => {
+            update(&args.path)
+        }
+        _ => {
+            writeln!(handle, "Unknown command: {}", args.command).context("Could not write to stdout")
+        }
+    }
 }
